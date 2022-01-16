eafa136e4be2d8c1f07dba3e6d0433ab972a44ee
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-16 14:14:28 +0900

    debug

--- script.js
+++ script.js
@@ -1,60 +1,24 @@
-// const currencyEl_one = document.getElementById('currency-one');
-// const amountEl_one = document.getElementById('amount_one');
-// const currencyEl_two = document.getElementById('currency-two');
-// const amountEl_two = document.getElementById('amount_two');
-// const rateEl = document.getElementById('rate');
-// const swap = document.getElementById('swap');
-
-
-
-// function calculate () {
-//     const currency_one = currencyEl_one.value;
-//     const currency_two = currencyEl_two.value;
-//     fetch("https://open.exchangerate-api.com/v6/latest")
-//     //fetch("https://v6.exchangerate-api.com/v6/478101dbc9d5eebcf7a92844/latest/USD")
-//         .then(res => res.json())
-//         .then(data => {
-//             const rate = data.rates[currency_two] / data.rates[currency_one];
-//             rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-//             amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
-//         });
-// }
-
-// // Event Listener
-// currencyEl_one.addEventListener('change', calculate);
-// amountEl_one.addEventListener('input', calculate);
-// currencyEl_two.addEventListener('change', calculate);
-// amountEl_two.addEventListener('input', calculate);
-
-// swap.addEventListener('click', () => {
-//     const temp = currencyEl_one.value;
-//     currencyEl_one.value = currencyEl_two.value;
-//     currencyEl_two.value = temp;
-//     calculate();
-// });
-
-// calculate();
-
 const currencyEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.getElementById('currency-two');
-const amountEl_two = document.getElementById('amount-two');
+const amountEl_one = document.getElementById('amount_one');
+const currencyEl_two = document.getElementById('currency_two');
+const amountEl_two = document.getElementById('amount_two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-function calculate() {
-  const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
-  fetch("https://open.exchangerate-api.com/v6/latest")
-    .then(res => res.json())
-    .then(data => {
-      //  console.log(data);
-      const rate = data.rates[currency_two] / data.rates[currency_one];
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
-    });
-}
 
+
+function calculate () {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    fetch("https://open.exchangerate-api.com/v6/latest")
+    //fetch("https://v6.exchangerate-api.com/v6/478101dbc9d5eebcf7a92844/latest/USD")
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.rates[currency_two] / data.rates[currency_one];
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+            amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+        });
+}
 
 // Event Listener
 currencyEl_one.addEventListener('change', calculate);
@@ -63,11 +27,10 @@ currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
 
 swap.addEventListener('click', () => {
-  const temp = currencyEl_one.value;
-  currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
-  calculate();
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
 });
-
 
 calculate();
